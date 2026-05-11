fa4927368a18a083903f08c50197bc6dbb35eadb
Squishy-111 <camrozendaal@gmail.com>
2026-05-11 10:39:32 -0600

    Add control flow outline for game_engine

---
+++ src/game_engine.cpp
@@ -0,0 +1,21 @@
+#include "game_engine.h"
+
+void game_engine::run(){
+
+// create deck instance
+// create both the human_player instance and the cpu_player instance
+// input name for human player
+// shuffle and deal deck
+
+// implement main game while loop checking for ultimate winner
+// check if both players hand is empty
+// refill cards if hand is empty for either player
+// flip cards
+// check for war
+// move won cards to winner's graveyard
+// check if both players hand is empty
+// refill cards if hand is empty for either player
+
+
+
+}
