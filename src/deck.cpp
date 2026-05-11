203ff35cfb3ba88f63fe656d14da27bdab61ebbf
Squishy-111 <camrozendaal@gmail.com>
2026-05-11 11:57:17 -0600

    Define shuffle method for Deck class

---
+++ src/deck.cpp
@@ -0,0 +1,39 @@
+#include "deck.h"
+
+#include <iostream>
+#include <random>
+
+/* The shuffle function starts by making a temp container, then drains the std::queue 
+for cards into the temp, then randomly pushes an item from temp back to the std::queue 
+and removes that item from temp, and does that until temp is empty. */
+
+void deck::shuffle(){
+
+    // temporary container for cards
+    std::vector<Card> temp {};
+
+    // drain cards into temp storage for random access
+    while(cards.empty() != true) {
+        temp.push_back(cards.front());
+        cards.pop();
+    }
+    
+    // grab random seed and initalize generator using Mersenne Twister
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    while(temp.empty() != true){
+
+        // generate number from 1 to length of vector both inclusive
+        std::uniform_int_distribution<> distr(0, temp.size() -1 );
+        int rand_index {distr(gen)};
+
+        cards.push(temp.at(rand_index));
+        temp.erase(temp.begin() + rand_index);
+
+    }
+
+    std::cout << "deck is shuffled";
+
+
+}
